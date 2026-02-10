3f7a4e090e34c6e6b0a393b242f7b48257f58f80
Gus Ballman <gus.ballman@wsu.edu>
2026-02-09 20:35:43 -0800

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,7 +1,21 @@
-/*
-  Programmer: Gus Ballman
-  Date: 2/9/2026
-*/
+/******************************************************************************
+ * File: kernel_main.c
+ * Description: Entry point for the Monolithic ARM Kernel.
+ *              Initializes essential peripherals.
+ *
+ * Functions:
+ *   - kernel_main(): Initializes the UART subsystem and enters
+ *                    the main kernel loop.
+ *
+ * Notes:
+ *   - This file is part of a collaborative project to build a 
+ *     simple monolithic ARM kernel.
+ *   - The main loop is currently empty and will be expanded
+ *     as additional kernel services are implemented.
+ *
+ * Authors: Gus Ballman, Jamie Scott
+ * Date: 2/8/2026
+ *****************************************************************************/
 
 #include "kernel_main.h"
 
@@ -10,7 +24,7 @@ void kernel_main(void)
     UART_Initial(); // Initialize UARTs
     while (1) // Main Kernel Loop
     {
-
+      //TODO -> Build kernel loop
     }
 
 }
