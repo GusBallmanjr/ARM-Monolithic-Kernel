ebe7daf5e2474726cf0b6772d4a64bedc9766579
MrThomas <jawisco@icloud.com>
2026-02-10 13:48:59 -0800

    Fixed Issues. Added stack push, pop features. Added memory block data.

--- memory_management/allocator.s
+++ memory_management/allocator.s
@@ -4,15 +4,12 @@
 @ List of functions
 
 .section .data
-.align 3
-
-free_list: .quad 0
+.align 4
+memory_pool: .skip 32768
+free_list: .word memory_pool
 
 .text
-.global bb_malloc
-.global bb_dealloc
-.global bb_calloc
-.global bb_realloc
+.global _start
 
 @ [~, x, y] -> [location]
 index:
@@ -24,63 +21,77 @@ index:
 @ [id] -> [location]
 @ Allocates the closest free block from 0 to the process @ id
 bb_malloc:
+  push {r4, lr}
+  ldr r4, =memory_pool
   mov r2, #0
+
   bbm_loop:
-    ldr r1, [r2]
+    ldr r1, [r4, r2]
     cmp r1, #0
     beq bbm_allocate
-    add r2, r2, #1
+    add r2, r2, #4
     cmp r2, #32768
     blt bbm_loop
     b bbm_finish
+
   bbm_allocate:
-    str r0, [r1]
+    str r0, [r4, r2]
+    add r0, r4, r2
+
   bbm_finish:
-    mov r2, #0
-    bl index
-    bx lr
+    pop {r4, pc}
 
 @ [id]
 @ Deallocates the furthest block from 0 of the id
 @ Deallocate means to set the process assigned to the block to 0
 bb_dealloc:
+  push {r4, lr}
+  ldr r4, =memory_pool
   mov r2, #32768
+
   bbd_loop:
-    ldr r1, [r2]
-    cmp r0, r2
+    ldr r1, [r4, r2]
+    cmp r1, r0
     beq bbd_deallocate
-    sub r2, r2, #1
-    cmp r2, #0
-    bgt bd_loop
+
+    subs r2, r2, #4
+    bge bd_loop
     b bbd_finish
+
   bbd_deallocate:
     mov r1, #0
-    str r1, [r2]
+    str r1, [r4, r2]
+
   bbd_finish:
-    bx lr
+    pop {r4, pc}
 
 @ [location]
 bb_calloc:
-  mov r2, #0
+  push {r4, r5, lr}
   bl index
-  add r2, r0, #32768
+  ldr r4, =memory_pool
+  add r4, r4, r0
+
+  mov r3, #0
+  mov r5, #0
+
   bbc_loop:
-    mov r3, #0
-    str r3, [r0]
-    add r0, r0, #1
-    cmp r0, r2
+    str r3, [r4, r5]
+    add r5, r5, #4
+    cmp r5, #32768
     blt bbc_loop
-  bx lr
+  
+    pop {r4, r5, pc}
 
 @ [size] -> [failed]
 bb_realloc:
   mov r1, #32768
   cmp r0, r1
-  bbr_true:
+  bhi bbr_failed
+
+  mov r0, #0
+  bx lr
+
+  bbr_failed:
     mov r0, #1
-    b bbr_finish
-  bbr_false:
-    mov r0, #0
-    b bbr_finish
-  bbr_finish:
     bx lr
